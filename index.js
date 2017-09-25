94924f2b57e1995c82e05c24ab7c25e824f35abf
David Grant <mrdavidgrant@outlook.com>
2017-09-25 20:08:50 +0000

    query working

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const { Client } = require('pg')
-const client = new Client()
+const pg        = require('pg')
+const settings  = require('./settings')
+let input       = process.argv[2].split('').join('')
 
-await client.connect()
+const client    = new pg.Client({
+  user:         settings.user,
+  password:     settings.password,
+  database:     settings.database,
+  host:         settings.hostname,
+  port:         settings.port,
+  ssl:          settings.ssl
+})
 
-const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-console.log(res.rows[0].message) // Hello world!
-await client.end()
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err)
+  }
+  client.query("SELECT * FROM famous_people WHERE first_name = $1", [input], (err, result) => {
+    if (err) {
+      return console.error("error running query", err)
+    }
+    console.log(`Found ${result.rows.length} person(s) by the name ${input}:`)
+    for (let row in result.rows) {
+      console.log(`- ${row*1 + 1}: ${result.rows[row].first_name} ${result.rows[row].first_name} born ${result.rows[row].birthdate}`)
+    }
+    client.end()
+  })
+})
