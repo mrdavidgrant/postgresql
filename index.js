5f18fc26dc7c90c9a5407a4f22a642ca2403a588
David Grant <mrdavidgrant@outlook.com>
2017-09-26 11:08:22 -0400

    refactored for proper DB handling

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-const pg        = require('pg')
-const settings  = require('./settings')
-let input       = process.argv[2].split('').join('')
 
-const client    = new pg.Client({
-  user:         settings.user,
-  password:     settings.password,
-  database:     settings.database,
-  host:         settings.hostname,
-  port:         settings.port,
-  ssl:          settings.ssl
-})
+const app         = require('./lib/apps')
 
-client.connect((err, db) => {
-  if (err) {
-    return console.error("Connection Error", err)
-  }
+const input       = process.argv.slice(2)
+const DataHelpers = require("./lib/data-helpers")()
 
-  const DataHelpers = require("./lib/data-helpers.js")(db)
-    console.log(`Searching database ....`)
-    DataHelpers.query(db, input)
-})
+console.log(`Searching database ....`)
 
-
-function logResults (result) {
-  console.log(`Found ${result.length} users with first name ${input}`)
-  for (let row in result) {
-    console.log(`- ${row * 0 + 1}: ${result[row].first_name} ${result[row].last_name} born ${result[row].birthdate.toLocaleDateString()}`)
-  }
-}
+let results = DataHelpers.query(input)
